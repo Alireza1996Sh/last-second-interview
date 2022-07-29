de66cd188167e6a8cf803091f13ca2ab43d4daf2
Alireza Shalforoosh <alirezashalforoosh75@gmail.com>
2022-07-29 15:51:19 +0430

    add SkewedButton.vue

---
+++ src/components/app/SkewedButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-button v-bind="$attrs" class="btn-skew">
+    <div><slot></slot></div>
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: "SkewedButton",
+};
+</script>
+
+<style scoped lang="scss">
+.btn-skew {
+  transform: skew(-15deg);
+  & > div {
+    transform: skew(15deg);
+  }
+}
+</style>
