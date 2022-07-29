1240be5a6a484ed4402873b47389277681ccd612
Alireza Shalforoosh <alirezashalforoosh75@gmail.com>
2022-07-29 12:41:50 +0430

    add layout plugin of bootstrap

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -1,7 +1,8 @@
 // global bootstrap components and icons (tree-shaking)
-import { BButton, BIconCart } from "bootstrap-vue";
+import { BButton, BIconCart, LayoutPlugin } from "bootstrap-vue";
 
 export function initBootstrapVue(Vue) {
   Vue.component("BButton", BButton);
   Vue.component("BIconCart", BIconCart);
+  Vue.use(LayoutPlugin);
 }
