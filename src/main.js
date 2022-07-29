0022e47bfc452564ef52e195b0486168ce348586
Alireza Shalforoosh <alirezashalforoosh75@gmail.com>
2022-07-29 12:41:08 +0430

    add mq package

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
+import VueMq from "vue-mq";
+
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
 import { initBootstrapVue } from "@/config/bootstrap";
 import "@/assets/scss/bootstrap.scss";
 
+Vue.use(VueMq);
 initBootstrapVue(Vue);
 
 Vue.config.productionTip = false;
