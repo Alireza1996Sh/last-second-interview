381c61a5af77bc433a8a17be21587fde0f4d925a
Alireza Shalforoosh <alirezashalforoosh75@gmail.com>
2022-07-29 17:47:41 +0430

    update SectionTitle to support subtitle props

--- src/components/app/SectionTitle.vue
+++ src/components/app/SectionTitle.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="d-flex justify-content-between align-items-center">
-    <div class="font-weight-bold">{{ title }}</div>
+    <div class="font-weight-bold">
+      {{ title }}
+      <span v-if="subtitle" class="text-secondary small">({{ subtitle }})</span>
+    </div>
     <div v-if="action">
       <b-button
         variant="transparent"
@@ -20,6 +23,10 @@ export default {
     title: {
       type: String,
       required: true,
+    },
+    subtitle: {
+      type: String,
+      default: () => "",
     },
     action: {
       type: Object,
